ff1ebce31ca9271c3f0fe31fc69afbc22f19eed5
Peter Griess <griessp@actbefore-lm.sl1.internal.xoopit.com>
2009-12-12 16:40:21 -0600

    AJAX page for /explorer; /api/cascade action.
    - Create jQuery-driven AJAX page at /explorer for exploring the Cascade
      JSON API. Proxies POST requests through /api/cascade and signs them
      using OAuth creds found in cookies.
    - Re-jigger all HTTP calls in webapp to use urllib2 to ease porting off
      of GAE at some point in the nebulous future.

--- main.py
+++ main.py
@@ -8,17 +8,36 @@
 #
 # NOTE: Need to figure out how to expunge keys from git history if making
 #       this open source. Or change keys and de-activate the old one.
+#
+# TODO:
+#
+#   - Generating request / response pairs using AJAX direct to the Cascade
+#     endpoint is impossible without building an OAuth implemetnation that
+#     can run entirely in JavaScript. We need to proxy through our web
+#     service.
+#
+#       - Because of this, our request is not going to be exactly as sent
+#         to the Cascade endpoint. If we want to get at this (do we?), we
+#         will need to send it back from the web service. Maybe use MXHR
+#         to get both request and response back using a single response.
+#
+#   - Replace lame explicit checks for cookies / auth with decorators.
+#     We probably need 2 varieties: one redirecting, one not.
+#
+#   - Use Django directly so that porting off of GAE is easier.
 
-from google.appengine.api.urlfetch import fetch 
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 import cgi
 import logging
 import oauth
+import os
 import pprint
 import simplejson
 import urllib
+import urllib2
 
 REQUEST_TOKEN_COOKIE_NAME = 'rt'
 ACCESS_TOKEN_COOKIE_NAME = 'at'
@@ -63,15 +82,17 @@ class OAuthInitHandler(OAuthHandler):
 
         oaReq.sign_request(self._oaSig, self._oaConsumer, None)
         logging.debug('Requet token request URL: "%s"', oaReq.to_url())
-        reqTokenResp = fetch(oaReq.to_url())
 
-        if reqTokenResp.status_code != 200:
-            logging.warning('Failed to get OAuth request token: "%s"', reqTokenResp.content)
+        reqTokenResp = urllib2.urlopen(oaReq.to_url())
+        reqTokenRespContent = '\n'.join(reqTokenResp.readlines())
+
+        if reqTokenResp.code != 200:
+            logging.warning('Failed to get OAuth request token: "%s"', reqTokenRespContent)
             self.response.set_status(403)
             return
 
-        logging.info('Got request token: ' + reqTokenResp.content)
-        oaReqToken = oauth.OAuthToken.from_string(reqTokenResp.content)
+        logging.info('Got request token: ' + reqTokenRespContent)
+        oaReqToken = oauth.OAuthToken.from_string(reqTokenRespContent)
 
         self.response.headers.add_header(
             u'Set-Cookie',
@@ -108,12 +129,12 @@ class OAuthFinishHandler(OAuthHandler):
             return
 
         reqTokenDict = dict(cgi.parse_qsl(urllib.unquote_plus(self.request.cookies[REQUEST_TOKEN_COOKIE_NAME])))
+        logging.debug('reqTokenDict: ' + pprint.pformat(reqTokenDict))
+
         if not u'oauth_token' in reqTokenDict:
             logging.warning('No "%s" key in the "%s" cookie.' % ('oauth_token', REQUEST_TOKEN_COOKIE_NAME))
             self.response.set_status(403)
             return
-
-        logging.debug('reqTokenDict: ' + pprint.pformat(reqTokenDict))
 
         if reqTokenDict[u'oauth_token'] != self.request.get('oauth_token'):
             logging.warning('Cookie and URL disagree about request token name')
@@ -144,16 +165,18 @@ class OAuthFinishHandler(OAuthHandler):
         )
 
         oaReq.sign_request(self._oaSig, self._oaConsumer, oaReqToken)
-        logging.debug('Access token request URL: %s', oaReq.to_url())
-        accTokenResp = fetch(oaReq.to_url())
+        logging.debug('Access token request URL: "%s"', oaReq.to_url())
 
-        if accTokenResp.status_code != 200:
-            logging.warning('Failed to get OAuth access token: "%s"', accTokenResp.content)
+        accTokenResp = urllib2.urlopen(oaReq.to_url())
+        accTokenRespContent = '\n'.join(accTokenResp.readlines())
+
+        if accTokenResp.code != 200:
+            logging.warning('Failed to get OAuth access token: "%s"', accTokenRespContent)
             self.response.set_status(403)
             return
 
-        logging.info('Got access token: ' + accTokenResp.content)
-        oaAccToken = oauth.OAuthToken.from_string(accTokenResp.content)
+        logging.info('Got access token: ' + accTokenRespContent)
+        oaAccToken = oauth.OAuthToken.from_string(accTokenRespContent)
        
         self.response.headers.add_header(
             u'Set-Cookie',
@@ -178,28 +201,72 @@ class OAuthFinishHandler(OAuthHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Access token: set')
 
-class APIHandler(webapp.RequestHandler):
-    '''The meat of the API goes here. Bounces through the OAuth token
-       acquisition dance if no credentials are found.'''
+class CascadeAPIHandler(OAuthHandler):
+    '''The Cascade API handler. Requires an authenticated session and
+       does not redirect to get one.'''
 
-    def get(self):
+    JSON_ENDPOINT_URL = u'http://mail.yahooapis.com/ws/mail/v1.1/jsonrpc'
+
+    def post(self):
         if not ACCESS_TOKEN_COOKIE_NAME in self.request.cookies:
-            self.redirect(
-                'http://www.yttrium.ws/auth/oauth/init?' + \
-                urllib.urlencode(
-                    [(u'url', self.request.url)]
-                )
-            )
+            logging.warning('No access token found in request')
+            self.response.set_status(403)
             return
 
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Access token: ok')
+        # Construct a signed URL. We can do this regardless of the POST
+        # data provided to us by the client.
+        accTokenDict = dict(cgi.parse_qsl(urllib.unquote_plus(self.request.cookies[ACCESS_TOKEN_COOKIE_NAME])))
+        logging.debug('accTokenDict: ' + pprint.pformat(accTokenDict))
+
+        if not u'oauth_token' in accTokenDict:
+            logging.warning('No "%s" key in the "%s" cookie.' % ('oauth_token', ACCESS_TOKEN_COOKIE_NAME))
+            self.response.set_status(403)
+            return
+
+        if not u'oauth_token_secret' in accTokenDict:
+            logging.warning('No "%s" key in the "%s" cookie.' % ('oauth_token_secret', ACCESS_TOKEN_COOKIE_NAME))
+            self.response.set_status(403)
+            return
+
+        oaAccToken = oauth.OAuthToken(
+            accTokenDict['oauth_token'],
+            accTokenDict['oauth_token_secret']
+        )
+        oaReq = oauth.OAuthRequest(
+            http_method = u'POST',
+            http_url = self.JSON_ENDPOINT_URL,
+            parameters = {
+                u'oauth_nonce' : oauth.generate_nonce(),
+                u'oauth_timestamp' : oauth.generate_timestamp(),
+                u'oauth_consumer_key' : self._oaConsumer.key,
+                u'oauth_token' : oaAccToken.key,
+                u'oauth_version' : u'1.0'
+            }
+        )
+        oaReq.sign_request(self._oaSig, self._oaConsumer, oaAccToken)
+        logging.debug('Cascade JSON request URL: "%s"', oaReq.to_url())
+
+        try:
+            cascadeReq = urllib2.Request(
+                url = oaReq.to_url(),
+                data = self.request.body,
+                headers = { 'Content-Type' : 'application/json' }
+            )
+            cascadeResp = urllib2.urlopen(cascadeReq)
+        except urllib2.HTTPError, e:
+            logging.debug(pprint.pformat(e))
+            cascadeResp = e
+
+        cascadeRespContent = '\n'.join(cascadeResp.readlines())
+
+        self.response.set_status(cascadeResp.code)
+        self.response.out.write(cascadeRespContent)
+        for hn, hv in cascadeResp.headers.items():
+            self.response.headers.add_header(hn, hv)
 
 class ExplorerHandler(webapp.RequestHandler):
     '''Explore the Cascade API.'''
 
-    JSON_ENDPOINT_URL = 'http://mail.yahooapis.com/ws/mail/v1.1/jsonrpc'
-
     def get(self):
         if not ACCESS_TOKEN_COOKIE_NAME in self.request.cookies:
             self.redirect(
@@ -210,15 +277,23 @@ class ExplorerHandler(webapp.RequestHandler):
             )
             return
 
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Access token: ok')
+        gtemplPath = os.path.join(
+            os.path.dirname(__file__),
+            'gtmpl',
+            'explorer.gtmpl'
+        )
+
+        self.response.out.write(webapp.template.render(gtemplPath, {}))
 
 def main():
+    # Configure log levels
+    logging.getLogger().setLevel(logging.DEBUG)
+
     app = webapp.WSGIApplication(
         [
             ('/auth/oauth/init', OAuthInitHandler),
             ('/auth/oauth/finish', OAuthFinishHandler),
-            ('/api', APIHandler),
+            ('/api/cascade', CascadeAPIHandler),
             ('/explorer', ExplorerHandler),
             ('/', MainHandler)
         ],
@@ -229,3 +304,5 @@ def main():
 
 if __name__ == '__main__':
   main()
+
+# vim:tabstop=4 shiftwidth=4 expandtab
